d357c36d0b9c04a266e0b78d23fa4b4b5addc7af
Glumo87 <vojtech.chlapek@gmail.com>
2023-02-27 14:05:44 +0100

    Initial commit

---
+++ src/common/Field.java
@@ -0,0 +1,21 @@
+package common;
+
+public interface Field {
+    boolean canMove();
+    MazeObject get();
+    boolean isEmpty();
+
+    boolean remove(MazeObject object);
+
+    boolean put(MazeObject object);
+
+    Field nextField(Field.Direction dirs);
+
+    void setMaze(Maze maze);
+    static enum Direction {
+        D,L,R,U;
+       /* public int deltaRow();
+        public int deltaCol();*/
+    }
+s
+}
---
+++ src/common/Maze.java
@@ -0,0 +1,8 @@
+package common;
+
+public interface Maze {
+    Field getField(int row, int col);
+    int numRows();
+    int numCols();
+
+}
